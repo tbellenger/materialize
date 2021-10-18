8a2af9adef1f4d0481d3718324b9ecf00c8cc50f
tbellenger <tbellenger@gmail.com>
2021-10-17 19:50:12 -0700

    modal testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 var sidenavInsts;
+var modalInsts;
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    sidenavInsts = M.Sidenav.init(elems, {'edge':'left'});
+    sidenavInsts = M.Sidenav.init(elems);
+    elems = document.querySelectorAll('.modal');
+    modalInsts = M.Modal.init(elems);
 });
